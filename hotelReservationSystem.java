e10d906f772a0d5d3ac2c17fa032626134f499d1
Ismail836 <disguiser836@gmail.com>
2025-03-04 08:32:01 +0500

    class reservation and override

--- hotelReservationSystem.java
+++ hotelReservationSystem.java
@@ -20,4 +20,26 @@ class Room {
     }
 }
 
+class Reservation {
+    int id;
+    Room room;
+    LocalDate in, out;
+    String name, email, status;
+    double total;
+
+    public Reservation(int id, Room room, LocalDate in, LocalDate out, String name, String email) {
+        this.id = id;
+        this.room = room;
+        this.in = in;
+        this.out = out;
+        this.name = name;
+        this.email = email;
+        this.status = "Pending";
+        this.total = room.price * ChronoUnit.DAYS.between(in, out);
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation ID: " + id + ", Room: " + room.number + ", Total: $" + total + ", Payment: " + status;
+    }
 }
