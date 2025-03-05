22ffc67fd1137949dc36d35f276ab5a99b542184
Ismail836 <disguiser836@gmail.com>
2025-03-05 23:54:35 +0500

    BuyStock func

--- StockTradingPlatform.java
+++ StockTradingPlatform.java
@@ -18,4 +18,16 @@ class Portfolio {
         this.stocks = new HashMap<>();
         this.balance = 10000; // Starting balance
     }
+
+    public void buyStock(String symbol, int quantity, double price) {
+        double cost = quantity * price;
+        if (cost > balance) {
+            System.out.println("Insufficient funds!");
+            return;
+        }
+        balance -= cost;
+        stocks.put(symbol, stocks.getOrDefault(symbol, 0) + quantity);
+        System.out.printf("Bought %d shares of %s at $%.2f per share.\n", quantity, symbol, price);
+    }
+
 }
